63aec5dfe78d85dfad5bfe37053070cd6ee28295
tivonse <tivonse0717@gmail.com>
2021-04-03 18:40:44 +0800

    commit basic endpoints providing mine_block for the blockchain using Flask

--- blockchain.py
+++ blockchain.py
@@ -61,6 +61,44 @@ class Blockchain:
 
 # Part 2 - Mining our Blockchain
 
+# Creating a web app
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+# Creating a blockchain
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {"message": "All good. The blockchain is valid"}
+    else:
+        response = {"message": "Hey, we have a problem. The Blockchain is not valid."}
+    return jsonify(response), 200
+
+# Running the app
+app.run(host = '0.0.0.0', port = 5000)
 
 
 
